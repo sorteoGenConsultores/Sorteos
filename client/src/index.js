c7b5c922d0b3664cb6e988ece8f9101cc89178ac
Joel301 <joel3.1416@gmail.com>
2023-03-15 16:11:35 -0600

    agregado codigo para routeo basico

--- client/src/index.js
+++ client/src/index.js
@@ -3,17 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {Provider} from 'react-redux';
-import {store} from './store';
+import { BrowserRouter } from "react-router-dom";
+
+// import {Provider} from 'react-redux';
+// import {store} from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <Provider store ={store}>
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  // </Provider>,
-  document.getElementById('root')
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 reportWebVitals();
