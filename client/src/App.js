1d71a940b913c6cbd07577217ec12e1e125ff743
TomasTinto1234 <tititinto@gmail.com>
2023-03-17 10:03:15 -0300

    actions

--- client/src/App.js
+++ client/src/App.js
@@ -20,21 +20,6 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route exact path='/' element={
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>} />
         <Route exact path="/Login" element={<Login />} />
         <Route exact path="/register" element={<Register />} />
         <Route exact path='/testing' element={<TestComponent />} />
--- client/src/redux/redux.js
+++ client/src/redux/redux.js
@@ -1,12 +1,16 @@
 
 const initialSate = {
+    participant: [],
     
 }
 
 
 function rootReducer(state = initialSate, action) {
     switch (action.type) {
-    
+        case 'CREATE_PARTICIPANT':
+            return {
+                ...state
+            }
         default:
             return state;
         }
