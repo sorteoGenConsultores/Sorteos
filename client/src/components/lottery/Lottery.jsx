5b60f2994db0799b613b537afec1ee48aa26d680
jfparrado <jnsparrado@hotmail.com>
2023-03-23 09:25:56 -0500

    se pone imagen en componente loteria

--- client/src/components/lottery/Lottery.jsx
+++ client/src/components/lottery/Lottery.jsx
@@ -5,7 +5,7 @@ import QuestionCards from '../questioncards/QuestionCards';
 function Lottery() {
   return (
     <div>
-      <img src="https://example.com/lottery-image.png" alt="Lottery" />
+      <img src="https://elordenmundial.com/wp-content/uploads/2020/01/loteria-portada.png" alt="Lottery" />
       <Roulette />
       <QuestionCards />
       <button>Register</button>
